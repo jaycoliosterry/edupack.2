b322960152223e8feba934c675042d7d83242cac
Prelang Builder <builder@prelang.com>
2017-03-23 15:58:30 +0000

    Generated model EconomyPrice with migration

---
+++ app/models/economy_price.rb
@@ -0,0 +1,2 @@
+class EconomyPrice < ActiveRecord::Base
+end
---
+++ db/migrate/20170323155830_create_economy_prices.rb
@@ -0,0 +1,22 @@
+class CreateEconomyPrices < ActiveRecord::Migration
+  def change
+    create_table :economy_prices do |t|
+      t.decimal :weight
+      t.decimal :zone1
+      t.decimal :zone2
+      t.decimal :zone3
+      t.decimal :zone4
+      t.decimal :zone5
+      t.decimal :zone6
+      t.decimal :zone7
+      t.decimal :zone8
+      t.decimal :zone9
+      t.decimal :zone10
+      t.decimal :zone11
+      t.decimal :zone12
+      t.decimal :zone13
+
+      t.timestamps
+    end
+  end
+end
